ccb936f5a0a92de0c8556bf094aa818c5eb70954
Duncan Crawford <duncan@tandemconsulting.eu>
2014-04-28 12:12:57 +0100

    increment to 1.05 and addition of release notes

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -7,7 +7,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   val nameApp = "simple-reactivemongo"
-  val versionApp = "1.0.5-SNAPSHOT"
+  val versionApp = "1.0.5"
 
   val appDependencies = {
     import Dependencies._
