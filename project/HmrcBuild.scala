18a614435fcb1f25090afe56e68d6aef9278df8c
Duncan Crawford <duncan@tandemconsulting.eu>
2014-05-19 08:51:09 +0100

    increment to snapshot version

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -7,7 +7,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   val nameApp = "simple-reactivemongo"
-  val versionApp = "1.1.0"
+  val versionApp = "1.2.0-SNAPSHOT"
 
   val appDependencies = {
     import Dependencies._
