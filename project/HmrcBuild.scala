4ba34eb9847fe6602d3a6042c0201abd2037d6fd
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-03 08:52:21 +0100

    increment nscala-time to 1.2.0

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -43,7 +43,7 @@ object Dependencies {
     val reactiveMongoJson = "uk.gov.hmrc" %% "reactivemongo-json" % "0.1.0-SNAPSHOT"
     val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % "0.10.0"
     val playJson = "com.typesafe.play" %% "play-json" % "[2.1.0,2.2.3]" % "provided"
-    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.0.0"
+    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.2.0"
   }
 
   sealed abstract class Test(scope: String) {
