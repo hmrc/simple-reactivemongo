d36501200e486c9de45b96070c756b6f2e185910
Duncan Crawford <duncan@tandemconsulting.eu>
2014-07-28 08:42:41 +0100

    refactor build to use sbt-utls 2.0.2

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -3,8 +3,10 @@ import Keys._
 
 object HmrcBuild extends Build {
 
-
   import uk.gov.hmrc.DefaultBuildSettings
+  import DefaultBuildSettings._
+  import BuildDependencies._
+  import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
 
   val nameApp = "simple-reactivemongo"
   val versionApp = "2.0.0-SNAPSHOT"
@@ -23,34 +25,40 @@ object HmrcBuild extends Build {
     )
   }
 
-  lazy val root = Project(nameApp, file("."), settings = DefaultBuildSettings(nameApp, versionApp, scalaversion = "2.11.1", targetJvm = "jvm-1.7")() ++ Seq(
-    libraryDependencies ++= appDependencies,
-    publishArtifact in Test := true,
-    resolvers := Seq(
-      Opts.resolver.sonatypeReleases,
-      Opts.resolver.sonatypeSnapshots,
-      "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/",
-      "typesafe-snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
-    ),
-    crossScalaVersions := Seq("2.11.1", "2.10.4")
-  ) ++ SonatypeBuild()
-  )
+  lazy val simpleReactiveMongo = Project(nameApp, file("."))
+    .settings(version := versionApp)
+    .settings(scalaSettings : _*)
+    .settings(defaultSettings() : _*)
+    .settings(
+      targetJvm := "jvm-1.7",
+      shellPrompt := ShellPrompt(versionApp),
+      libraryDependencies ++= appDependencies,
+      resolvers := Seq(
+        Opts.resolver.sonatypeReleases,
+        Opts.resolver.sonatypeSnapshots,
+        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/",
+        "typesafe-snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+      ),
+      crossScalaVersions := Seq("2.11.2", "2.10.4")
+    )
+    .settings(SbtBuildInfo(): _*)
+    .settings(SonatypeBuild(): _*)
 
 }
 
 object Dependencies {
 
   object Compile {
-    val reactiveMongoJson = "uk.gov.hmrc" %% "reactivemongo-json" % "1.0.0-SNAPSHOT" cross CrossVersion.binary
-    val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % "0.10.5.akka23-SNAPSHOT" cross CrossVersion.binary
-    val playJson = "com.typesafe.play" %% "play-json" % "[2.1.0,2.3.1]" % "provided" cross CrossVersion.binary
-    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.2.0" cross CrossVersion.binary
+    val reactiveMongoJson = "uk.gov.hmrc" %% "reactivemongo-json" % "1.0.0"
+    val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % "0.10.5.akka23-SNAPSHOT"
+    val playJson = "com.typesafe.play" %% "play-json" % "[2.1.0,2.3.2]" % "provided"
+    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.2.0"
   }
 
   sealed abstract class Test(scope: String) {
 
-    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.0" % scope cross CrossVersion.binary
-    val pegdown = "org.pegdown" % "pegdown" % "1.4.2" % scope cross CrossVersion.Disabled
+    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.0" % scope
+    val pegdown = "org.pegdown" % "pegdown" % "1.4.2" % scope
   }
 
   object Test extends Test("test")
