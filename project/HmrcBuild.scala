227694930d4839bfed1c3888612a692c29847584
Duncan Crawford <duncan@noughtsandones.co.uk>
2015-09-14 11:21:04 +0100

    increment reactivemongo-json

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -40,11 +40,11 @@ object HmrcBuild extends Build {
 object Dependencies {
 
   object Compile {
-    val reactiveMongoJson = "uk.gov.hmrc" %% "reactivemongo-json" % "2.1.3"
+    val reactiveMongoJson = "uk.gov.hmrc" %% "reactivemongo-json" % "2.2.0"
     //NOTE: 0.11.6 Netty 3.10.4.Final clashes with Play (2.3.9) version of Netty 3.9.8
     val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % "0.11.5"
     val playJson = "com.typesafe.play" %% "play-json" % "2.3.9" % "provided"
-    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.8.0"
+    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "2.2.0"
     val logback = "ch.qos.logback" % "logback-classic" % "1.1.2"
   }
 
