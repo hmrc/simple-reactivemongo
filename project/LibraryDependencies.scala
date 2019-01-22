8c5daf5a1fe71178e59805f23a43fe3d3fd4ea84
Konrad Wudkowski <11072914+konradwudkowski@users.noreply.github.com>
2019-01-22 13:30:27 +0000

    Upgrade play to 2.5.19

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibraryDependencies {
 
-  private val play25Version = "2.5.12"
+  private val play25Version = "2.5.19"
   private val play26Version = "2.6.20"
 
   val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
@@ -13,33 +13,30 @@ object LibraryDependencies {
     play25 = Seq(
       "org.slf4j"         % "slf4j-api"                % "1.7.21",
       "com.typesafe.play" %% "play"                    % play25Version,
-      "com.typesafe.play" %% "play-json"               % "2.5.12",
       "org.reactivemongo" %% "reactivemongo-play-json" % "0.16.0-play25"
     ),
     play26 = Seq(
       "org.slf4j"         % "slf4j-api"                % "1.7.25",
       "com.typesafe.play" %% "play"                    % play26Version,
       "com.typesafe.play" %% "play-guice"              % play26Version,
-      "com.typesafe.play" %% "play-json"               % "2.6.10",
       "org.reactivemongo" %% "reactivemongo-play-json" % "0.16.0-play26"
     )
   )
 
   val test: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
-      "com.outworkers" %% "util-samplers" % "0.40.0" % Test,
-      "org.pegdown"    % "pegdown"        % "1.6.0"  % Test,
-      "org.scalacheck" %% "scalacheck"    % "1.13.4" % Test,
-      "org.scalamock"  %% "scalamock"     % "4.1.0"  % Test,
-      "org.scalatest"  %% "scalatest"     % "3.0.5"  % Test
+      "com.outworkers" %% "util-samplers"  % "0.40.0" % Test,
+      "org.pegdown"    % "pegdown"         % "1.6.0"  % Test,
+      "org.scalacheck" %% "scalacheck"     % "1.13.4" % Test,
+      "org.scalamock"  %% "scalamock"      % "4.1.0"  % Test,
+      "org.scalatest"  %% "scalatest"      % "3.0.5"  % Test,
+      "ch.qos.logback" % "logback-classic" % "1.2.3"  % Test
     ),
     play25 = Seq(
-      "ch.qos.logback"         % "logback-classic"     % "1.1.2"       % Test,
       "com.typesafe.play"      %% "play-test"          % play25Version % Test,
       "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"       % Test
     ),
     play26 = Seq(
-      "ch.qos.logback"         % "logback-classic"     % "1.2.3"       % Test,
       "com.typesafe.play"      %% "play-test"          % play26Version % Test,
       "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"       % Test
     )
