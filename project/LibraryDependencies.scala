d39e24eb64d34c1057515b1304e1b0d0779d4706
Konrad Wudkowski <wudkowski@gmail.com>
2019-01-22 11:34:18 +0000

    PLATOPS-1902: Upgrade ReactiveMongo to 0.16.1
    This fixes the performance problem with reconnecting present
    in 0.15.1 and 0.16.0.

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -8,7 +8,7 @@ object LibraryDependencies {
   val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
       "com.github.nscala-time" %% "nscala-time"   % "2.2.0",
-      "org.reactivemongo"      %% "reactivemongo" % "0.16.0"
+      "org.reactivemongo"      %% "reactivemongo" % "0.16.1"
     ),
     play25 = Seq(
       "org.slf4j"         % "slf4j-api"                % "1.7.21",
